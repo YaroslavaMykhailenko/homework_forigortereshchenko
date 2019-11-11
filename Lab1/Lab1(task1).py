bbb02a8f441ee477a493b7a6bb01890fe08dbdac
Slava <sla.va@icloud.com>
2019-11-11 22:54:44 +0300

    Homework_For_IgorTereshchenko

---
+++ Lab1/Lab1(task1).py
@@ -0,0 +1,15 @@
+# 1 task
+'''
+Вивести на екран прямокутник, заповнений літерами А. Кількість рядків в прямокутнику дорівнює 5, кількість стовпців дорівнює 8.
+'''
+import re
+i = 1
+word = input("Enter your string:")
+result = re.split(r'[B-Zb-z]', word)
+if len(result)==1:
+    for i in range(5):
+        for i in range(8):
+            print(result[0], end="")
+        print()
+else:
+    print("Incorrect string")
